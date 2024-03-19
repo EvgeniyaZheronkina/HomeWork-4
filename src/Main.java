ed625b1b06bfc1d0f524bf93f969ecb62b9f7705
Евгения <zheronkina86@gmail.com>
2024-03-19 22:17:07 +0300

    Всё сделано

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,55 @@ public class Main {
         } else {
             System.out.println( year + " год не является высокосным");
         }
+
+
+        //Задача 4
+        System.out.println("Задача 4");
+
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " +deliveryDays+ "срок доставки");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            deliveryDays++;
+            System.out.println("Потребуется дней: " +deliveryDays);
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            deliveryDays = deliveryDays + 2;
+            System.out.println("Потребуется дней: " +deliveryDays);
+        } else {
+            System.out.println("Доставки нет");
+        }
+
+
+        //Задача 5
+        System.out.println("Задача 5");
+
+        int monthNumber = 13;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                break;
+        }
     }
 }
